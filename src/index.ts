4859d5de2be255a3eeb416cdd211f6c6dc76b369
C G Giridhar <girigov39@gmail.com>
2026-02-06 12:10:06 +0100

    fix(exports): add exports

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { ShaclParser } from './shacl/shacl-parser';
+export { ShaclDocument } from './shacl/shacl-document';
+export {
+  IntermediateRepresentationBuilder,
+  IntermediateRepresentation,
+} from './ir/intermediate-representation-builder';
+export { IrSchemaConverter } from './json-schema/ir-schema-converter';
+export { ConversionOptions } from './json-schema/conversion-options';
