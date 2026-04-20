8c2289c7e157dd7c7edf88ad684aee222d3179b9
C G Giridhar <girigov39@gmail.com>
2026-04-20 17:18:50 +0200

    chore(benchmark): fix qualified value shape processing

--- src/shacl/tree-processor/edge/contains-edge-processor.ts
+++ src/shacl/tree-processor/edge/contains-edge-processor.ts
@@ -1,5 +1,6 @@
 import { SchemaEdge } from '../../../tree/types';
 import {
+  SHACL_NODE,
   SHACL_QUALIFIED_MAX_COUNT,
   SHACL_QUALIFIED_MIN_COUNT,
   SHACL_QUALIFIED_VALUE_SHAPE,
@@ -18,19 +19,44 @@ export class ContainsEdgeProcessor implements EdgeProcessor {
     return edges.filter((e) => e.label === 'contains');
   }
 
-  process({ edges, subject, schema }: EdgeContext): ChildNode[] {
+  process({ edges, subject, isBlank, schema }: EdgeContext): ChildNode[] {
     if (subject == null) return [];
     if (schema == null || schema.length === 0) return [];
     const [edge] = edges;
-    const blankId = this.context.nextBlankId();
-    this.context.store.triple(subject, SHACL_QUALIFIED_VALUE_SHAPE, blankId, true);
-    this.shaclMapper.map(edge.node.schema, blankId, true);
+    const qualifiedBlankId = this.context.nextBlankId();
+
+    if (isBlank) {
+      this.context.store.bothBlank(subject, SHACL_QUALIFIED_VALUE_SHAPE, qualifiedBlankId);
+    } else {
+      this.context.store.triple(subject, SHACL_QUALIFIED_VALUE_SHAPE, qualifiedBlankId, true);
+    }
+
+    const containsSchema = edge.node.schema;
+    if (containsSchema.$ref) {
+      this.context.store.blank(
+        qualifiedBlankId,
+        SHACL_NODE,
+        this.context.resolveRef(containsSchema.$ref)
+      );
+    } else {
+      this.shaclMapper.map(containsSchema, qualifiedBlankId, true);
+    }
 
     const minContains = schema.minContains ?? 1;
-    this.context.store.literalInt(subject, SHACL_QUALIFIED_MIN_COUNT, minContains, false);
+    this.context.store.literalInt(
+      subject,
+      SHACL_QUALIFIED_MIN_COUNT,
+      minContains,
+      isBlank ?? false
+    );
 
     if (schema.maxContains != null) {
-      this.context.store.literalInt(subject, SHACL_QUALIFIED_MAX_COUNT, schema.maxContains, false);
+      this.context.store.literalInt(
+        subject,
+        SHACL_QUALIFIED_MAX_COUNT,
+        schema.maxContains,
+        isBlank ?? false
+      );
     }
     return [];
   }
